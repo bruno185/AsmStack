b5bc4682ecda3355587dcfce48fe9eb95c0a3103
salut.lespotes@outlook.fr <salut.lespotes@outlook.fr>
2020-08-22 00:55:01 +0200

    comment 4

--- AsmStack/main.asm
+++ AsmStack/main.asm
@@ -17,12 +17,12 @@ main PROC
 main ENDP
 
 mysub PROC
-	push ebp			; sera dans esp+4 
+	push ebp			; sera dans esp+4 (après adresse de retour)
 	mov ebp,esp
-	sub esp,8			; place pour variables lcales
+	sub esp,8			; espace pour variables lcales
 
 	mov eax,[ebp+8]		; récuoère argument 1
-	mov [esp],eax		; le place dans variable  1
+	mov [esp],eax		; le place dans variable 1
 	mov eax, 77778888h
 	mov [esp+4],eax		; place eax dans variable locale 2
 
